95b213c84a605a32dced1457ec35d8d3fddd3856
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-26 13:36:11 +0700

    set input with bt

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
-    <b-form @submit.prevent="onFormSubmit">
-      <b-form-group
-        id="input-group-userId"
-        label="User Id:"
-        label-for="userId"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="userId"
-          v-model="userId"
-          type="text"
-          placeholder="Enter user"
-        ></b-form-input>
-      </b-form-group>
-      <input id="password" v-model="password" type="password" />
-      <button type="submit">Login</button>
-    </b-form>
-    <button id="register" @click="onRegisterClick">Register new account</button>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="12" sm="12" md="6" lg="4" xl="2">
+          <b-form @submit.prevent="onFormSubmit">
+            <b-form-group
+              id="input-group-userId"
+              label="User Id:"
+              label-for="userId"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input id="userId" v-model="userId" type="text" placeholder="Enter user"></b-form-input>
+            </b-form-group>
+            <b-form-group id="input-group-password" label="Password:" label-for="password">
+              <b-form-input
+                id="password"
+                v-model="password"
+                type="password"
+                placeholder="Enter Password"
+              ></b-form-input>
+            </b-form-group>
+            <button type="submit">Login</button>
+          </b-form>
+        </b-col>
+      </b-row>
+      <button id="register" @click="onRegisterClick">Register new account</button>
+    </b-container>
   </div>
 </template>
 
