24a2bf37993a8902df41adee8fa96ab00675805b
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-27 10:12:55 +0700

    add Auth Store

---
+++ src/stores/auth.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const Auth = {
+  state: {},
+  actions: {
+    async login(user) {
+      return new Promise((resolve, reject) => {
+        axios
+          .post("http://localhost:3000/login", user)
+          .then(resp => resolve(resp.data))
+          .catch(reason => reject(reason));
+      });
+    }
+  }
+};
+
+export default Auth;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,7 +13,9 @@
               <b-form-input
                 id="userId"
                 v-model="$v.userForm.userId.$model"
-                :state="$v.userForm.userId.$dirty ? !$v.userForm.userId.$error : null"
+                :state="
+                  $v.userForm.userId.$dirty ? !$v.userForm.userId.$error : null
+                "
                 type="text"
                 placeholder="Enter user"
               ></b-form-input>
@@ -21,11 +23,19 @@
                 >This is a required field.</b-form-invalid-feedback
               >
             </b-form-group>
-            <b-form-group id="input-group-password" label="Password:" label-for="password">
+            <b-form-group
+              id="input-group-password"
+              label="Password:"
+              label-for="password"
+            >
               <b-form-input
                 id="password"
                 v-model="$v.userForm.password.$model"
-                :state="$v.userForm.password.$dirty ? !$v.userForm.password.$error : null"
+                :state="
+                  $v.userForm.password.$dirty
+                    ? !$v.userForm.password.$error
+                    : null
+                "
                 type="password"
                 placeholder="Enter Password"
               ></b-form-input>
@@ -75,18 +85,7 @@ export default {
     },
     onFormSubmit() {
       if (!this.$v.userForm.$invalid) {
-        axios
-          .post("http://localhost:3000/login", this.userForm)
-          .then(resp => {
-            if (resp.data.success) {
-              this.$router.replace("/admin/user/list");
-            } else {
-              alert("warning " + resp.data.msg);
-            }
-          })
-          .catch(reason => {
-            alert("error " + reason);
-          });
+        // todo call login action from auth
       } else {
         alert("Please do something");
       }
