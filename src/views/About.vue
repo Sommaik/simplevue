b1e2ffd2f52d12398ce21881c6c0f9d691f0b9f0
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-27 08:58:05 +0700

    add support img

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-container fluid="">
+      <b-card-group deck>
+        <b-card
+          title="Card Title"
+          img-src="@/assets/download.jpeg"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card
+          title="Card Title"
+          img-src="@/assets/logo.png"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+    </b-container>
   </div>
 </template>
